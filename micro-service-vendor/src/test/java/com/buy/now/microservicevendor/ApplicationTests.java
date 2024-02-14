edb3bf1df7bf92a43badb2d6a664c9a3ef37f7cd
Ahtisham Latif <ahtisham.latif@paysyslabs.com>
2024-02-14 16:54:23 +0500

    Initial commit

---
+++ micro-service-vendor/src/test/java/com/buy/now/microservicevendor/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buy.now.microservicevendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
